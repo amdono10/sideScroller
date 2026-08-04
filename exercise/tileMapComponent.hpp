c296353b40ba7808fc2833fdf855f29ae0f40b10
Adam Donovan <donoam@gmail.com>
2026-08-04 17:09:55 -0400

    WIP

---
+++ exercise/tileMapComponent.hpp
@@ -0,0 +1,16 @@
+
+#ifndef _TILE_MAP_COMPONENT
+#define _TILE_MAP_COMPONENT
+
+#include "spriteComponent.hpp"
+
+class TileMapComponent : public SpriteComponent
+{
+public:
+
+private:
+
+};
+
+
+#endif	// _TILE_MAP_COMPONENT
